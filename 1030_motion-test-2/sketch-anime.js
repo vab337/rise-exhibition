ca1ec21527850b978cfaa38f808d5a9cacb04f6a
Ven Bui <vab337@nyu.edu>
2023-10-30 14:34:28 +0700

    motion test 2

---
+++ 1030_motion-test-2/sketch-anime.js
@@ -0,0 +1,39 @@
+var t1 = anime.timeline({
+    easing: 'easeOutQuart',
+    duration: 5000,
+    loop: true,
+  });
+
+
+//   rise motion
+  t1
+  // .add({
+  //   targets: '#container',
+  //   rotateX: [90,-30],
+  //   rotateY: [0, -45],
+  // })
+  // .add({
+  //   targets: '.cube',
+  //   translateY: [1000,0],
+  //   delay: anime.stagger(100),
+  // });
+
+  
+
+
+
+
+
+
+
+
+
+//   .add({
+//     targets: '#container',
+//     keyframes: [
+//         {translateY: 0},
+//         {translateY: -100},
+//       ],
+//   });
+
+
